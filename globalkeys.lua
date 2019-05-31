802f9ac4eb2890a0d3ed0f6bb75042b773b2ed06
madand <dev@madand.net>
2019-05-31 20:29:58 +0300

    Use keycode for screen cycling hotkeys

--- globalkeys.lua
+++ globalkeys.lua
@@ -27,10 +27,6 @@ globalkeys = gtable.join(
     {description = "swap with next client by index", group = "client"}),
   awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
     {description = "swap with previous client by index", group = "client"}),
-  awful.key({ modkey,           }, "o", function () awful.screen.focus_relative( 1) end,
-    {description = "focus the next screen", group = "screen"}),
-  awful.key({ modkey, "Shift"   }, "o", function () awful.screen.focus_relative(-1) end,
-    {description = "focus the previous screen", group = "screen"}),
   awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
     {description = "jump to urgent client", group = "client"}),
   awful.key({ modkey,           }, "Tab",
@@ -91,7 +87,11 @@ globalkeys = gtable.join(
     {description = "Focus the screen #2", group = "screen"}),
   awful.key({modkey}, "F3",     function () awful.screen.focus(3) end,
     {description = "Focus the screen #3", group = "screen"}),
-
+  -- Keycode 39 represents letter "s" in QWERTY and "o" in Dvorak layout respectively.
+  awful.key({ modkey,           }, "#39", function () awful.screen.focus_relative( 1) end,
+    {description = "focus the next screen", group = "screen"}),
+  awful.key({ modkey, "Shift"   }, "#39", function () awful.screen.focus_relative(-1) end,
+    {description = "focus the previous screen", group = "screen"}),
 
   awful.key({}, "XF86ScreenSaver", function () awful.spawn.with_shell("loginctl suspend") end)
 )
